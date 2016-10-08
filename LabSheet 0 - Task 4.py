37b3e59668e448a88776e75803df4b76497976a0
Nadeem-Doc <doctorwm@uni.coventry.ac.uk>
2016-10-08 14:12:24 +0100

    Update LabSheet 0 - Task 4.py

--- LabSheet 0 - Task 4.py
+++ LabSheet 0 - Task 4.py
@@ -1,6 +1,10 @@
-#x =  input ("Input a word: ")
-x = "ben"
-x= x.replace ("e","rbhasretn")
-print(x)
+word =  input ("Input a word: ")
+start = int(input ("select starting point: "))
+remove = int(input ("select starting point: "))
 
+count=1
+while count!= remove:
+    word= word.replace (word[start],"")
+    count=count+1
 
+print(word)
