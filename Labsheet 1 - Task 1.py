29372baeae4cdc2bbb946e5d338f7e02a3eb8873
Nadeem-Doc <doctorwm@uni.coventry.ac.uk>
2016-10-19 16:30:01 +0100

    Update Labsheet 1 - Task 1.py

--- Labsheet 1 - Task 1.py
+++ Labsheet 1 - Task 1.py
@@ -1,22 +1,23 @@
-"""
-include real docstrings and correct variable names
-"""
-
 import random
 
-x = int(input ("first: "))
-y = int(input ("first: "))
-t = int(input ("first: "))
-f = int(input ("first: "))
-d = int(input ("first: "))
+No1 = int(input ("First Number: "))
+No2 = int(input ("Second Number: "))
+No3 = int(input ("Third Number: "))
+No4 = int(input ("Forth Number: "))
+No5 = int(input ("Fifth Number: "))
 
-listOfInput = [x,y,t,f,d]
-
+listOfInput = [No1,No2,No3,No4,No5]
+"""
+ListShuffler takes listOfInput and uses is the lenght of the list as a range
+then genereates two random numbers which are used to randomly swap two elements in the list
+returns the random list
+"""
 def ListShuffler(listOfInput):
-    for i in range(len(listOfInput)):
+    for number in range(len(listOfInput)):
         randomNumber1 = random.randrange(0,4)
         randomNumber2 = random.randrange(0,4)
         listOfInput[randomNumber1], listOfInput[randomNumber2] = listOfInput[randomNumber2], listOfInput[randomNumber1]
     return listOfInput
+
 
 print(ListShuffler(listOfInput))
