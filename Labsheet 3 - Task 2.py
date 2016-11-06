2d762918cf920136201031f5ec0957058b41279e
Nadeem-Doc <doctorwm@uni.coventry.ac.uk>
2016-11-06 19:35:21 +0000

    Update Labsheet 3 - Task 2.py

--- Labsheet 3 - Task 2.py
+++ Labsheet 3 - Task 2.py
@@ -3,16 +3,19 @@
 recursively goes through the numbers below ‘prime’. if the base case is reached
 then the number is a prime. if at any stage the prime number to be checked is
 divisible by ‘n’ then 'prime' isn’t a prime number.
-
 """
-def primeCheck (prime, n):
+
+def _primeCheck (prime, n):
     if n == 1:
         return True
     elif prime%n == 0:
         return False
     else:
-        return primeCheck(prime, n-1)
+        return _primeCheck(prime, n-1)
+
+def primeCheck( prime ): #initialises the parameters for  _primeCheck 
+    return _primeCheck( prime, prime -1 )# what is the _ used for?
 
 prime = 21
 #prime is set to takeaway 1 to that the elif statment isnt triggered 
-print( primeCheck(prime, prime-1))
+print( primeCheck(prime))
