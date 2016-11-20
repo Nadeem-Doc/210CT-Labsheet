8ef2702c1ea3b0ba962f52c403e3dd48d74314bf
Nadeem-Doc <doctorwm@uni.coventry.ac.uk>
2016-11-20 21:17:44 +0000

    Update Labsheet 1 - Task 2.py

--- Labsheet 1 - Task 2.py
+++ Labsheet 1 - Task 2.py
@@ -1,16 +1,18 @@
+"""
+Takes the user input as the factorial. when the 'No'
+reaches a point where its divisible by five count is incremented.
+"""
 
-def trailInFactorial (x):
+def trailInFactorial (factorial):
     count = 0
-    for No in range (1,x+1):
+    for No in range (1,factorial+1):
         while No> 0:
             if No%5==0:
-                No = No/5
+                No = No/5 #No will continue where it was left off in fo loop    
                 count+= 1
             else:
                 break
     return count
 
-
 userInput = int(input ("Input Number: "))
-
 print(trailInFactorial(userInput))
